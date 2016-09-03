536a1f059eb7892344d214c761c7ee6772ced426
Alex Schumann <rubin@zebrafish.org>
2016-09-02 17:50:55 -0700

    First commit, vol6 issue1

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a newsletter for a school group and a script to make it work
+
+
+Only interesting if you are a member, or you would like to do something similar for your own group.
+
+
+newsletter.html - html that will be converted into an email
+send.php - php script (command line php) to send the email formatted properly to be html email
+
+
---
+++ send.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once('libphp-phpmailer/class.phpmailer.php');
+
+$to = '';
+if(count($argv) > 1) {
+  $to = $argv[1];
+  echo "DEBUG: Set to to $to\n";
+}
+else {
+   echo "Usage: {$argv[0]} <to>\n";
+   exit;
+}
+
+$mail             = new PHPMailer(); // defaults to using php "mail()"
+
+$body             = file_get_contents('newsletter.html');
+$body             = eregi_replace("[\]",'',$body);
+
+#$mail->SetFrom('rubin@afternet.org', 'Rubin');
+$mail->AddAddress("$to");
+$mail->Subject = "Holt Quick News";
+$mail->MsgHTML($body);
+
+if(!$mail->Send()) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+  echo "Message sent!";
+}
+echo "\n";
+    
