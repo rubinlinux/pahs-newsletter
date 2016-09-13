5a5975489337a21865f1bd8dc387a42114f293e9
Alex Schumann <rubin@zebrafish.org>
2016-09-13 09:58:08 -0700

    make send handle filename as an argument, tweak spanish template a bit, add text-version to email

--- send.php
+++ send.php
@@ -1,20 +1,28 @@
 <?php
 
 require_once('libphp-phpmailer/class.phpmailer.php');
+require_once("class.html2text.inc");
 
 $to = '';
-if(count($argv) > 1) {
-  $to = $argv[1];
-  echo "DEBUG: Set to to $to\n";
+$file = '';
+if(count($argv) > 2) {
+  $file = $argv[1];
+  $to = $argv[2];
 }
 else {
-   echo "Usage: {$argv[0]} <to>\n";
+   echo "Usage: {$argv[0]} <file.html> <to>\n";
    exit;
 }
 
 $mail             = new PHPMailer(); // defaults to using php "mail()"
+$mail->CharSet = 'utf-8';
 
-$body             = file_get_contents('newsletter-v6i1.html');
+$body             = file_get_contents($file);
+if(!$body) {
+   echo "ERROR: file $file doesn't seem to contain anything\n";
+}
+
+# Protect the email from harmful char - this could be a problem.. 
 $body             = eregi_replace("[\]",'',$body);
 
 $mail->SetFrom('rubin@afternet.org', 'Alex Schumann');
@@ -22,10 +30,17 @@ $mail->AddAddress("$to");
 $mail->Subject = "Holt Quick News";
 $mail->MsgHTML($body);
 
+$h2t = &new html2text($body);
+$text = $h2t->get_text();
+
+$mail->AltBody = $text;
+
 if(!$mail->Send()) {
   echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
   echo "Message sent!";
 }
 echo "\n";
+
+
     
