048b4793fdb25fa227d950442756ce6c0e25264d
Alex Schumann <rubin@zebrafish.org>
2016-09-26 19:29:09 -0700

    update send.php to be a little more advanced

--- send.php
+++ send.php
@@ -5,17 +5,30 @@ require_once("class.html2text.inc");
 
 $to = '';
 $file = '';
-if(count($argv) > 2) {
-  $file = $argv[1];
-  $to = $argv[2];
-}
-else {
-   echo "Usage: {$argv[0]} <file.html> <to>\n";
-   exit;
+#if(count($argv) > 2) {
+#  $file = $argv[1];
+#  $to = $argv[2];
+#}
+#else {
+
+function usage() {
+   global $argv;
+   echo "Usage: {$argv[0]} --file=file.html [--send=to@address] [--text]\n";
 }
 
-$mail             = new PHPMailer(); // defaults to using php "mail()"
-$mail->CharSet = 'utf-8';
+$opts = getopt("", array("text", "send:", "file:"));
+
+if(!array_key_exists("file", $opts)) {
+   usage();
+   exit;
+}
+$file = $opts['file'];
+
+if(array_key_exists("send", $opts)) {
+   $to = $opts['send'];
+}
+
+$mail;
 
 $body             = file_get_contents($file);
 if(!$body) {
@@ -25,20 +38,32 @@ if(!$body) {
 # Protect the email from harmful char - this could be a problem.. 
 $body             = eregi_replace("[\]",'',$body);
 
-$mail->SetFrom('rubin@afternet.org', 'Alex Schumann');
-$mail->AddAddress("$to");
-$mail->Subject = "Holt Quick News";
-$mail->MsgHTML($body);
-
 $h2t = &new html2text($body);
 $text = $h2t->get_text();
 
-$mail->AltBody = $text;
+if($to) {
+   $mail             = new PHPMailer(); // defaults to using php "mail()"
+   $mail->CharSet = 'utf-8';
 
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-  echo "Message sent!";
+   $mail->SetFrom('rubin@afternet.org', 'Alex Schumann');
+   $mail->AddAddress("$to");
+   $mail->Subject = "Holt Quick News";
+   $mail->MsgHTML($body);
+   $mail->AltBody = $text;
+
+   if(!$mail->Send()) {
+     echo "Mailer Error: " . $mail->ErrorInfo;
+   } else {
+     echo "Message sent!";
+   }
+}
+else {
+   if(array_key_exists('text', $opts)) {
+      echo $text;
+   }
+   else {
+      echo $body;
+   }
 }
 echo "\n";
 
